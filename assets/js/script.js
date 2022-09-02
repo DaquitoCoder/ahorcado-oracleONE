066e04e375cad001b66dc0f4ec7e0884cc361e82
Juan Alejandro León <66072912+DaquitoCoder@users.noreply.github.com>
2022-09-02 15:19:26 -0500

    new: detect the correct letters in the word

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@ const buttonIniciarJuego = document.getElementById('btn-start');
 const buttonAddWord = document.getElementById('btn-addword');
 
 let palabras = ["ARROZ", "POLLO", "CAMION", "GATO", "PERRO", "CABALLO", "CAMISA", "PANTALON", "ZAPATO", "CAMARA", "CELULAR", "COMPUTADORA", "MESA", "SILLA", "SOFA"];
+const palabra = palabras[Math.floor(Math.random() * palabras.length)];
+
+let palabraLetrasAdivinadas = [];
+palabraLetrasAdivinadas.length = palabra.length;
 
 let fallos = 0;
 
@@ -40,8 +44,6 @@ const iniciarJuego  = () => {
   menuPrincipal.style.display = 'none';
   juego.style.display = 'flex';
 
-  const palabra = palabras[Math.floor(Math.random() * palabras.length)];
-
   console.log(palabra);
 
   const lineas = document.getElementsByClassName('lineas-letras')[0];
@@ -53,4 +55,47 @@ const iniciarJuego  = () => {
   }
 
   lineas.innerHTML = lineasUwU;
+}
+
+
+const detectarLetra = () => {
+  const textareaLetra = document.getElementById('intento-palabra');
+  if(textareaLetra.value == ''){
+    alert('Ingresa una letra');
+  }else{
+    const letra = textareaLetra.value[0].toUpperCase();
+    textareaLetra.value = '';
+    const lineas = document.getElementsByClassName('lineas-letras')[0];
+    let lineasUwU = "";
+    lineas.innerHTML = "";
+
+    if(palabra.indexOf(letra) > -1){
+      console.log('está')
+      for (let i = 0; i < palabra.length; i++) {
+        if(letra == palabra[i]){
+          palabraLetrasAdivinadas[i] = letra;
+        }
+      }
+
+      for (let i = 0; i < palabraLetrasAdivinadas.length; i++) {
+        if(palabraLetrasAdivinadas[i]){
+          lineasUwU += palabraLetrasAdivinadas[i] + " ";
+        }else{
+          lineasUwU += "_" + " ";
+        }
+      }
+
+      lineas.innerHTML = lineasUwU;
+
+      console.log(lineasUwU);
+      console.log(palabraLetrasAdivinadas);
+
+    }else{
+      console.log('no está')
+      fallos += 1
+    }
+    console.log(fallos)
+  }
+  
+  
 }
