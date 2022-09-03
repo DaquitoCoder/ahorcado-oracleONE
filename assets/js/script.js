0044722a6a389371e20d2a5f3bebd3da9261e440
Alejandro León <juan.aleon2001@gmail.com>
2022-09-02 21:51:54 -0500

    new: released the game, pending of new bugs
    released the game, i have to do the responsive design of the app

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,23 @@
 const buttonIniciarJuego = document.getElementById('btn-start');
 const buttonAddWord = document.getElementById('btn-addword');
 
-let palabras = ["ARROZ", "POLLO", "CAMION", "GATO", "PERRO", "CABALLO", "CAMISA", "PANTALON", "ZAPATO", "CAMARA", "CELULAR", "COMPUTADORA", "MESA", "SILLA", "SOFA"];
+let palabras = [
+  'ARROZ',
+  'POLLO',
+  'CAMION',
+  'GATO',
+  'PERRO',
+  'CABALLO',
+  'CAMISA',
+  'PANTALON',
+  'ZAPATO',
+  'CAMARA',
+  'CELULAR',
+  'COMPUTADORA',
+  'MESA',
+  'SILLA',
+  'SOFA',
+];
 const palabra = palabras[Math.floor(Math.random() * palabras.length)];
 
 let palabraLetrasAdivinadas = [];
@@ -10,92 +26,127 @@ palabraLetrasAdivinadas.length = palabra.length;
 let fallos = 0;
 
 const mostrarAddPalabra = () => {
-  const anadirPalabraDiv  = document.getElementsByClassName('anadir-palabra')[0];
+  const anadirPalabraDiv = document.getElementsByClassName('anadir-palabra')[0];
   const menuPrincipal = document.getElementsByClassName('botones-iniciales')[0];
 
   menuPrincipal.style.display = 'none';
   anadirPalabraDiv.style.display = 'flex';
-}
+};
 
 const cancelar = () => {
-  const anadirPalabraDiv  = document.getElementsByClassName('anadir-palabra')[0];
+  const anadirPalabraDiv = document.getElementsByClassName('anadir-palabra')[0];
   const menuPrincipal = document.getElementsByClassName('botones-iniciales')[0];
 
   menuPrincipal.style.display = 'flex';
   anadirPalabraDiv.style.display = 'none';
-}
+};
 
 const addPalabra = () => {
   const textarea = document.getElementById('palabraNueva');
   const palabra = textarea.value;
-  if(palabra.length > 0){
+  if (palabra.length > 0) {
     palabras.push(palabra.toUpperCase());
+    alert('Palabra agregada correctamente!');
     textarea.value = '';
-  }else{
+    cancelar();
+    iniciarJuego();
+  } else {
     alert('Ingrese una palabra');
-    console.log(palabras);
   }
-}
+};
 
-const iniciarJuego  = () => {
+const iniciarJuego = () => {
+  alert('Tienes 7 intentos para adivinar la palabra');
   const menuPrincipal = document.getElementsByClassName('botones-iniciales')[0];
   const juego = document.getElementsByClassName('tablero-juego')[0];
+  const imgAhorcado = document.getElementById('img-ahorcado');
+
+  imgAhorcado.src = '';
+  imgAhorcado.style.display = 'none';
 
   menuPrincipal.style.display = 'none';
   juego.style.display = 'flex';
 
-  console.log(palabra);
-
   const lineas = document.getElementsByClassName('lineas-letras')[0];
   const lineasPalabra = palabra.length;
-  let lineasUwU = "";
+  let lineasUwU = '';
 
-  for(let i = 0; i < lineasPalabra; i++){
-    lineasUwU += "_" + " ";
+  for (let i = 0; i < lineasPalabra; i++) {
+    lineasUwU += '_' + ' ';
   }
 
   lineas.innerHTML = lineasUwU;
-}
-
+};
 
 const detectarLetra = () => {
   const textareaLetra = document.getElementById('intento-palabra');
-  if(textareaLetra.value == ''){
+  if (textareaLetra.value == '') {
     alert('Ingresa una letra');
-  }else{
+  } else {
     const letra = textareaLetra.value[0].toUpperCase();
     textareaLetra.value = '';
-    const lineas = document.getElementsByClassName('lineas-letras')[0];
-    let lineasUwU = "";
-    lineas.innerHTML = "";
 
-    if(palabra.indexOf(letra) > -1){
-      console.log('está')
+    if (palabra.indexOf(letra) > -1) {
+      const lineas = document.getElementsByClassName('lineas-letras')[0];
+      let lineasUwU = '';
+      lineas.innerHTML = '';
       for (let i = 0; i < palabra.length; i++) {
-        if(letra == palabra[i]){
+        if (letra == palabra[i]) {
           palabraLetrasAdivinadas[i] = letra;
         }
       }
 
       for (let i = 0; i < palabraLetrasAdivinadas.length; i++) {
-        if(palabraLetrasAdivinadas[i]){
-          lineasUwU += palabraLetrasAdivinadas[i] + " ";
-        }else{
-          lineasUwU += "_" + " ";
+        if (palabraLetrasAdivinadas[i]) {
+          lineasUwU += palabraLetrasAdivinadas[i] + ' ';
+        } else {
+          lineasUwU += '_' + ' ';
         }
       }
-
       lineas.innerHTML = lineasUwU;
 
-      console.log(lineasUwU);
-      console.log(palabraLetrasAdivinadas);
+      if (juegoGanado()) {
+        setTimeout(() => {
+          alert('Ganaste! Felicitaciones!');
+          location.reload();
+        }, 1000);
+      }
 
-    }else{
-      console.log('no está')
-      fallos += 1
+    } else {
+      fallos += 1;
+      mostrarImagen();
     }
-    console.log(fallos)
   }
-  
-  
+};
+
+const mostrarImagen = () => {
+  const imgAhorcado = document.getElementById('img-ahorcado');
+  imgAhorcado.style.display = 'block';
+  imgAhorcado.src = 'assets/' + fallos + '.png';
+
+  if (fallos == 7) {
+    alert('Perdiste! Inténtalo de nuevo');
+    location.reload();
+  }
 }
+
+const juegoGanado = () => {
+  if(isArrayFull(palabraLetrasAdivinadas)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isArrayFull( arr )
+{
+  for ( var i = 0, l = arr.length; i < l; i++ )
+  {
+    if ( 'undefined' == typeof arr[i] || null === arr[i] )
+    {
+      return false
+    }
+  }
+  return true;
+}
+
